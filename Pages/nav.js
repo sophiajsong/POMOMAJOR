d13f4c39f48670f2dcd4fae13567b8ec53fb7d52
Sophia Song <sophiajsong06@gmail.com>
2023-10-09 23:47:32 -0700

    worked on pomodoro timer tab-- still WIP

---
+++ Pages/nav.js
@@ -0,0 +1,10 @@
+// JavaScript source code
+
+function openNav() {
+    document.getElementById("side_navbar").style.width = "200px";
+
+}
+
+function closeNav() {
+    document.getElementById("side_navbar").style.width = "0px";
+}
